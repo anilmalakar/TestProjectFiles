3f84a4f90578f4678c16b8c832dc1c4b37ef3713
anilmalakar <anilmalakar@gmail.com>
2018-12-09 21:16:18 -0500

    Adding test library for sonar analysis

---
+++ BizLib.Test/BizDataTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BizLib.Test
+{
+    [TestClass]
+    public class BizDataTest
+    {
+        [TestMethod]
+        public void GetData_Should_Return()
+        {
+        }
+    }
+}
---
+++ UILib.Test/UILibTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BizLib;
+using Moq;
+using UILib;
+
+namespace UILib.Test
+{
+    [TestClass]
+    public class UILibTest
+    {
+        Mock<IBizData> _bizData;
+        UI uiInstance;
+        [TestInitialize]
+        public void InIt()
+        {
+            _bizData = new Mock<IBizData>();
+
+        }
+
+        [TestMethod]
+        public void UI_Constructor_Test()
+        {
+            uiInstance = new UI(_bizData.Object);
+            Assert.IsNotNull(uiInstance);
+        }
+
+        [TestMethod]
+        public void UI_Should_Return_Data()
+        {
+            _bizData.Setup(s => s.GetData()).Returns("This is test");
+            uiInstance = new UI(_bizData.Object);
+            var result = uiInstance.GetData();
+            Assert.IsNotNull(uiInstance);
+            Assert.AreEqual(result, "This is test");
+        }
+    }
+}
